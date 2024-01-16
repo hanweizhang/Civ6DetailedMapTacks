ddd520c5648c8a4b9fc177078baa44f142f85a41
Jamie-nyanchi <80130176+JamieNyanchi@users.noreply.github.com>
2024-01-16 10:09:11 -0500

    Fix compatibility regressions from previous update
    Fixes compatibility issues with some mods, such as Production Queue, by copying over DeepCompare instead of including SupportFunctions.lua

--- ui/dmt_mappinsubjectmanager.lua
+++ ui/dmt_mappinsubjectmanager.lua
@@ -12,7 +12,6 @@
 -- =======================================================================
 -- Imports
 -- =======================================================================
-include("SupportFunctions"); -- DeepCompare
 include("dmt_serialize"); -- Serialize and Deserialize
 
 -- =======================================================================
@@ -156,4 +155,65 @@ function BroadcastMapPinSubjects()
     -- Broadcast MapPinSubjects to all clients
     Network.BroadcastPlayerInfo();
     bUnbroadcastedChanges = false;
+end
+
+-- DeepCompare copied from SupportFunctions.lua
+-- Some other mods unintentionally remove this function from that file, so it is copied here instead
+-- ===========================================================================
+--  Recursively compare two tables (ignoring metatable)
+--  Original from: https://stackoverflow.com/questions/25922437/how-can-i-deep-compare-2-lua-tables-which-may-or-may-not-have-tables-as-keys
+--  ARGS:    table1        table one
+--           table2        table two
+--  RETURNS:    true if tables have the same content, false otherwise
+-- ===========================================================================
+function DeepCompare( table1, table2 )
+    local avoid_loops = {}
+
+    local function recurse(t1, t2)      
+        -- Compare value types
+        if type(t1) ~= type(t2) then return false; end
+
+        -- Compare simple values
+        if type(t1) ~= "table" then return (t1 == t2); end
+      
+        -- First, let's avoid looping forever.
+        if avoid_loops[t1] then return avoid_loops[t1] == t2; end
+        avoid_loops[t1] = t2;
+
+            -- Copy keys from t2
+        local t2keys = {}
+        local t2tablekeys = {}
+        for k, _ in pairs(t2) do
+            if type(k) == "table" then table.insert(t2tablekeys, k); end
+            t2keys[k] = true;
+        end
+
+        -- Iterate keys from t1
+        for k1, v1 in pairs(t1) do
+            local v2 = t2[k1]
+            if type(k1) == "table" then
+                -- if key is a table, we need to find an equivalent one.
+                local ok = false
+                for i, tk in ipairs(t2tablekeys) do
+                    if DeepCompare(k1, tk) and recurse(v1, t2[tk]) then
+                        table.remove(t2tablekeys, i)
+                        t2keys[tk] = nil
+                        ok = true
+                        break;
+                    end
+                end
+                if not ok then return false; end
+            else
+                -- t1 has a key which t2 doesn't have, fail.
+                if v2 == nil then return false; end
+                t2keys[k1] = nil
+                if not recurse(v1, v2) then return false; end
+            end
+        end
+        -- if t2 has a key which t1 doesn't have, fail.
+        if next(t2keys) then return false; end
+        return true;
+    end
+
+    return recurse(table1, table2);
 end
